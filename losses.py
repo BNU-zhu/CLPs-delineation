fdaec05310ee64dc6834bab7f045efc70b056499
BNU-zhu <155819272+BNU-zhu@users.noreply.github.com>
2024-03-27 11:36:53 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -50,13 +50,12 @@ def weighted_cross_entropy_loss(inputs, label):
     """
     label = label.long()
     mask = label.float()
-    num_positive = torch.sum((mask > 0.5).float()).float() # ==1.
+    num_positive = torch.sum((mask > 0.5).float()).float()
     num_negative = torch.sum((mask == 0).float()).float()
 
     mask[mask == 1] = 1.0 * num_negative / (num_positive + num_negative)
     mask[mask == 0] = 1.1 * num_positive / (num_positive + num_negative)
     mask[mask == 2] = 0
-    inputs= torch.sigmoid(inputs)
     cost = torch.nn.functional.binary_cross_entropy(
             inputs.float(),label.float(), weight=mask, reduce=False)
 
